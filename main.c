7d3b0289ca92631c2cf54cd7c65688d9d9c40eca
Dakota Teall <code9654@outlook.com>
2016-07-07 22:51:22 -0400

    Simplified loops, added comments.

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-int currency;
-int infantry;
-int cavalry;
-char purchase;
-char difficulty;
+
 int main()
 {
-    while (difficulty != 'e' || difficulty != 'E' || difficulty != 'n' || difficulty != 'N' || difficulty != 'h' || difficulty != 'H'){
+    char purchase;
+    int cavalry = 0;
+    int infantry = 0;
+    int currency = 0;
+    char difficulty;
+
+    //Continues to loop until the user enters a correct difficulty. Informs user of incorrect selection.
+    //As it is impossible for currency = 1, the loop continues until it meets the requirements for the break.
+    while (currency != 1){
     printf("Please Select Difficulty\n[E] = Easy\n[N] = Normal\n[H] = Hard\n");
     scanf(" %c", &difficulty);
 
@@ -15,6 +19,7 @@ int main()
         else printf("\nInvalid difficulty selected, please try again\n");
        }
 
+    //Selects the difficulty based on the previous input.
     switch(difficulty){
 
     case 'e': case 'E':
@@ -32,6 +37,8 @@ int main()
         printf("\nHard mode selected, you may spend %d on units\n\n", currency);
         break;
     }
+
+    //Loops purchasing options until the user runs out of currency.
     printf("Please purchase your units. Your balance is %d\n\n[I]nfantry costs 500, [C]avalry costs 1000\n", currency);
     while (currency > 0){
     scanf(" %c", &purchase);
@@ -41,18 +48,15 @@ int main()
         (infantry ++);
         printf("\nInfantry purchased, your balance is %d. You control %d Infantry and %d Cavalry\n", currency, infantry, cavalry);
         continue;
-    }
+        }
     else if (purchase == 'c' && currency >= 1000 || purchase == 'C' && currency >= 1000 ){
         (currency -= 1000);
         (cavalry ++);
         printf("\nCavalry purchased, your balance is %d. You control %d Infantry and %d Cavalry\n", currency, infantry, cavalry);
         continue;
-    }
-    else if (purchase != 'i' || purchase != 'I' || purchase != 'c' || purchase != 'C'){
-        printf("\nInvalid purchase selected, please try again\n");
-        continue;
-    }
-        if (currency <= 0) break;
+        }
+    else printf("\nInvalid purchase selected, please try again\n");
+
     }
     printf("\n\nPurchase completed.");
     exit(0);
